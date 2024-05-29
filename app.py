5020098e3f1a5e55f5f4554c90a01889770cdcee
Muhammed Swavaf <47553718+swavafup@users.noreply.github.com>
2024-05-29 10:44:34 +0400

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def fetch_data_from_host(ip_address, port=22):
         ssh_client = paramiko.SSHClient()
         ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh_client.connect(hostname=ip_address, port=port, username=DEFAULT_USERNAME, password=DEFAULT_PASSWORD)
-        stdin, stdout, stderr = ssh_client.exec_command('welcome')
+        stdin, stdout, stderr = ssh_client.exec_command('pyspark')
         data = stdout.read().decode()
         ssh_client.close()
         return data
