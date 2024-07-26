06990b2b8a629fbc4c0924b58dfb2163ddcb0bd1
Muhammed Swavaf <47553718+swavafup@users.noreply.github.com>
2024-07-26 09:41:31 +0400

    Update app.py

--- app.py
+++ app.py
@@ -189,8 +189,8 @@ def main():
     with st.sidebar:
         # st.markdown("<div class='form-container'>", unsafe_allow_html=True)
         st.markdown("<h2>Login</h2>", unsafe_allow_html=True)
-        login_email = st.sidebar.text_input('Please enter your email address', value="admin@castlab.com", disabled=False)
-        login_password = st.sidebar.text_input('Please enter your password',type = 'password', value="admin@castlab", disabled=False)
+        login_email = st.sidebar.text_input('Please enter your email address', value="", disabled=False)
+        login_password = st.sidebar.text_input('Please enter your password',type = 'password', value="", disabled=False)
         login = st.sidebar.checkbox('Login')
 
         # st.markdown("<a href='#' id='forgot-password-link'>Forgot password?</a>", unsafe_allow_html=True)
